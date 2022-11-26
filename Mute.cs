2d0f073e72696ee5deec383f7b5846f99fea1eeb
VilaterGRIEF <107187957+VilaterGRIEF@users.noreply.github.com>
2022-11-26 16:37:17 +0300

    Create Mute.cs

---
+++ Mute.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mute : MonoBehaviour
+{
+    private bool isMute;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMute = PlayerPrefs.GetInt("Mute") == 1;
+        AudioListener.pause = isMute;
+    }
+
+    // Update is called once per frame
+    public void IsMute()
+    {
+        isMute = !isMute;
+        AudioListener.pause = isMute;
+        PlayerPrefs.SetInt("Mute", isMute ? 1 : 0);
+    }
+}
